a86e0e379b4187f49fab9961374a636b2a5a9d52
Riad <riad@web-vision.de>
2023-09-19 09:33:50 +0200

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+$EM_CONF['academic_bite_jobs'] = [
+    'title' => 'Academic Bite Jobs',
+    'description' => '',
+    'category' => 'fe,be',
+    'state' => 'beta',
+    'version' => '1.0.0',
+];
